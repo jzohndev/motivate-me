31c788e088c2719a74462a0e201c2c7768fb30fa
jzohndev <big.gums@Hotmail.com>
2015-11-24 21:33:00 -0600

    Same Night Second Commit
    >Tested setContentView inside a TextView button to navigate between
    activities
    >Tested application interaction and TextView positioning

--- app/src/main/java/com/github/jzohndev/motivate_me/MainMenu.java
+++ app/src/main/java/com/github/jzohndev/motivate_me/MainMenu.java
@@ -4,14 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
+    private TextView iJustAte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.main_menu_activity);
+
+        iJustAte = (TextView) findViewById(R.id.ate_tvb);
+        addMeal();
     }
+
+    public void addMeal(){
+        iJustAte.setOnClickListener(
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    setContentView(R.layout.meals_list);
+            }
+        });
+    }
+
+
+
+
+
 
 
 
